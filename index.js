0fcc445cb15a8261bf023a859e9f9eea0fb7b03e
Michael Lackey <michael.g.lackey@gmail.com>
2021-02-03 15:01:53 -0600

    write jest tests

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 const joinStrings = (str1, str2) => {
-  // Return a string with both strings combined. You must include a space when joining both strings
+  // Return a string with both strings combined. You MUST include a space between the two strings when joining.
+
 }
 
 const absoluteSum = (arrNums) => {
-  // Return the absolute sum of all integers in an array
+  // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
-}
+  // Remember to check the type of the element
 
-const createObject = (arr1, arr2) => {
-  //  create an object utilizing the elements in arr1 and arr2
-  // Arr1 contains the keys
-  // Arr2 contains the object values
-  // Both arrays contain the same number of elements
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
+
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
+  
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
+
 }
 
-const findExponentation = (num, pow) => {
+const returnPower = (num, pow) => {
   // return the provided num to the power provided
-}
 
-// const
+}
 
 module.exports = {
   joinStrings,
   absoluteSum,
-  createObject
+  returnAllValues,
+  countItems,
+  returnAllEvens,
+  returnPower
 }
