035685b5127e4cdf70306cd24f053e093dc4eb26
Zitai Huang <mywpiedu@gmail.com>
2021-08-06 11:13:20 -0500

    1

--- assessment-part-2/script.js
+++ assessment-part-2/script.js
@@ -1,1 +1,58 @@
 // Insert Javascript Here
+console.log('hello')
+
+const button1 = document.querySelector('.button1')
+const inputs = document.querySelector('.inputs')
+const button2 = document.querySelector('.button2')
+const display = document.querySelector('.result')
+let result = 0
+inputs.value = 1
+
+button2.addEventListener('click', (button) => {
+  let values = parseInt(inputs.value)
+  console.log(values)
+  result += values
+  display.innerHTML = result
+  //bonus
+  if (result < 0) {
+    display.style.color = '#ff0000'
+    console.log(display)
+  } else if (result > 0) {
+    display.style.color = '#000000'
+  }
+})
+
+button1.addEventListener('click', (button) => {
+  let values = parseInt(inputs.value)
+  result -= values
+  display.innerHTML = result
+
+  if (result < 0) {
+    display.style.color = '#ff0000'
+    console.log(display)
+  } else if (result > 0) {
+    display.style.color = '#000000'
+  }
+})
+// mouse events to change button color
+button1.addEventListener('mouseover', (event) => {
+  let button = event.target
+  button.style.backgroundColor = '#ffffff'
+  button.style.color = 'black'
+})
+button1.addEventListener('mouseleave', (event) => {
+  let button = event.target
+  button.style.backgroundColor = '#808080'
+  button.style.color = 'white'
+})
+
+button2.addEventListener('mouseover', (event) => {
+  let button = event.target
+  button.style.backgroundColor = '#ffffff'
+  button.style.color = 'black'
+})
+button2.addEventListener('mouseleave', (event) => {
+  let button = event.target
+  button.style.backgroundColor = '#808080'
+  button.style.color = 'white'
+})
--- index.js
+++ index.js
@@ -2,34 +2,55 @@
 
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
-
+  let answer = str1 + ' ' + str2
+  return answer
 }
 
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
-
+  let sum = 0
+  for (let i = 0; i < arrNums.length; i++) {
+    if (typeof arrNums[i] === 'number') {
+      sum += Math.abs(arrNums[i])
+    }
+  }
+  return sum
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
+  //let answer = []
+  let answer = Object.keys(object).map((keys) => {
+    return object[keys]
+  })
 
+  return answer
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
-
+  let answer = nums.map((num) => {
+    if (num % 2 == 0) {
+      return num
+    }
+  })
+  return answer
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
-
+  let answer = 1
+  for (let i = 0; i < pow; i++) {
+    answer *= num
+  }
+  return answer
 }
 
 module.exports = {
