755849f4c1c9387498147577ad6f24c88743db9f
Hayden Anderson <anderson.hayden@gmail.com>
2021-08-17 10:00:50 -0400

    assessment resubmission

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
 
+  let string = str1 + ' ' + str2
+  return string
 }
 
 const absoluteSum = (arrNums) => {
@@ -10,26 +12,46 @@ const absoluteSum = (arrNums) => {
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
 
+  let sumArr = []
+
+  const numsOnly = arrNums.filter((nums) => {
+    if (typeof nums === 'number') {
+      sumArr.push(nums)
+    }
+  })
+
+  const sum = sumArr.reduce((accumulator, value) => {
+    return accumulator + value
+  }, 0)
+  return sum
 }
 
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
-
+  return Object.values(object)
 }
 
 const countItems = (arr) => {
   // return total number of items in the provided array
-  
+  return arr.length
 }
 
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
+  const evenArray = []
 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evenArray.push(nums[i])
+    }
+  }
+  return evenArray
 }
 
 const returnPower = (num, pow) => {
   // return the provided num to the power provided
 
+  return Math.pow(num, pow)
 }
 
 module.exports = {
